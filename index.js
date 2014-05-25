1088e84d318fc56b706d1502e12812d4e635b4ed
Dominic Pettifer <sironfoot@gmail.com>
2014-05-25 22:02:00 +0100

    added: initial implementation with unit tests

--- index.js
+++ index.js
@@ -1,3 +1,44 @@
+var path = require('path');
+
 module.exports = function(options) {
-    return null;
+
+    options = options || {};
+    var rootDir = options.rootDir || '/';
+
+    return function(modulePath) {
+        // deal with root paths (start with slash/)
+        if (modulePath.search(/^~?\/.+$/) === 0) {
+            modulePath = path.join(rootDir, modulePath.replace(/^~?(\/.+)$/, '$1')); // remove ~ char
+        }
+        // deal with local paths (no slash)
+        else {
+            var origPrepareStackTrace = Error.prepareStackTrace;
+
+            Error.prepareStackTrace = function (_, stack) { return stack; };
+            var stack = new Error().stack;
+            Error.prepareStackTrace = origPrepareStackTrace;
+
+            var caller = null;
+
+            for (var i = 0; i < stack.length; i++) {
+                var receiver = stack[i].receiver;
+                if (receiver) {
+                    var isModule = receiver.exports && receiver.filename;
+                    if (isModule) {
+                        caller = receiver;
+                        break;
+                    }
+                }
+            }
+
+            if (!caller) {
+                throw new Error('Unable to determine the caller');
+            }
+
+            var callerPath = path.dirname(caller.filename);
+            modulePath = path.join(callerPath, modulePath);
+        }
+
+        return require(modulePath);
+    };
 };
--- test/tests.js
+++ test/tests.js
@@ -1,41 +1,68 @@
 var assert = require('assert');
 
 var loquire = require('..');
-
-before(function() {
-    global.local = loquire({ rootDir: __dirname });
-});
+global.local = loquire({ rootDir: __dirname });
 
 describe('root paths', function() {
-    it('should require a module at root', function() {
-        var utils = local('/module');
+    it('should require a module at root (/module)', function() {
+        var module = local('/module');
 
-        assert(utils);
-        assert.equal(utils.testValue, 10);
+        assert(module);
+        assert.equal(module.testValue, 10);
     });
 
-    it('should require a module in a directory', function() {
-        var utils = local('/lib/module');
+    it('should require a module in a directory (/lib/module)', function() {
+        var module = local('/lib/module');
 
-        assert(utils);
-        assert.equal(utils.testValue, 11);
+        assert(module);
+        assert.equal(module.testValue, 11);
     });
 
-    it('should require a module in a sub directory', function() {
-        var utils = local('/lib/sub/module');
+    it('should require a module in a sub directory (/lib/sub/module)', function() {
+        var module = local('/lib/sub/module');
 
-        assert(utils);
-        assert.equal(utils.testValue, 12);
+        assert(module);
+        assert.equal(module.testValue, 12);
     });
 
-    it('should require a folder module (loading the default index.js)', function() {
-        var utils = local('/lib');
+    it('should require a folder module, loading the default index.js (/lib)', function() {
+        var module = local('/lib');
 
-        assert(utils);
-        assert.equal(utils.testValue, 13);
+        assert(module);
+        assert.equal(module.testValue, 13);
     });
 });
 
-describe('local paths', function() {
 
+
+describe('local paths', function() {
+    var localFile = local('module');
+    var localFolder = local('lib');
+
+    it('should require a local file (module)', function() {
+        assert(localFile);
+        assert.equal(localFile.testValue, 10);
+    });
+
+    it('should require a local folder, loading the default index.js (lib)', function() {
+        assert(localFolder);
+        assert.equal(localFolder.testValue, 13);
+    });
+
+    // here we identified a problem where the caller module could not be determined
+    // if the local-require was called from within an executing function
+    describe('called from executing function', function() {
+
+        it('should require a local file (module)', function() {
+            var module = local('module');
+            assert(module);
+            assert.equal(module.testValue, 10);
+        });
+
+        it('should require a local folder (lib)', function() {
+            var module = local('lib');
+            assert(module);
+            assert.equal(module.testValue, 13);
+        });
+    });
 });
